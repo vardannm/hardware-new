50b41d09334977dfe6a8705604e6d91c0a06026c
vardan.torosyan <vardan.torosyan@daniam.am>
2026-02-11 16:29:38 +0400

    layout update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import ScrollManager from "@/components/layout/scroll-restoration";
+import Script from "next/script";
+
 export const metadata = {
   title: "Hardware Projects",
   description: "Basic informative site for the shop",
@@ -11,7 +13,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-50 font-gilroy" suppressHydrationWarning>
-                <ScrollManager />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7MMNDMV7VV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7MMNDMV7VV');
+          `}
+        </Script>
+
+        <ScrollManager />
         <Navbar />
         <main className="min-h-[80vh]">{children}</main>
         <Footer />
